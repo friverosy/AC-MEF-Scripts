fe5e78549459b95c2f2cd3bc71d7ec8dd19e29a2
Cristtopher Quintana T <cquintana@patagoniawings.com>
2017-03-29 16:09:34 -0300

    remove person if already exist

--- updateMongoFromXml.py
+++ updateMongoFromXml.py
@@ -10,7 +10,7 @@ import slackweb
 import requests
 
 
-DIR = "/Users/cristtopher/Desktop/xmls/"
+DIR = "/opt/marcasmef/"
 LAST_EMPLOYEES = "lastEmployees.txt"
 LAST_CONTRACTORS = "lastContractors.txt"
 SERVER = "http://0.0.0.0"
@@ -39,7 +39,7 @@ def saveLastFile(text, last_file):
     try:
         file = open(last_file, "a")
         file.write(text + "\n")
-        print "file saved with ", text
+        #print "file saved with ", text
     except IOError as io:
         print "Error en saveLastFile()"
         print last_file + " -> " + io.strerror
@@ -60,19 +60,19 @@ def areEquals(new, last_file):
     # Compare files
     print "Comparing " + new + " with " + last
     if last == new:
-        print "Are equals"
+        #print "Are equals"
         return True
     else:
-        print "Are not equals"
+        #print "Are not equals"
         return False
 
 
 def sendGet(profile):
-    print ENDPOINT + profile
+    #print ENDPOINT + profile
     request = urllib2.Request(ENDPOINT + profile)
     try:
         response = urllib2.urlopen(request)
-        print response
+        #print response
         if (response.getcode() == 200):
             print "Instruction sent"
         else:
@@ -82,7 +82,7 @@ def sendGet(profile):
 
 
 def get_db():
-    client = MongoClient('localhost:27017')
+    client = MongoClient('0.0.0.0:27017')
     db = client.AccessControl20
     return db
 
@@ -92,16 +92,16 @@ def add_place(db, name, rut):
 
 
 def add_company(db, name, rut):
-    db.company.update({"name": name},{'$set': {"name" : name, "_id": rut}}, upsert=True)
+    db.company.update({"name": name},{"name" : name, "_id": rut}, upsert=True)
 
 
 def add_person(db, run, fullname, card, company_code, company, place, profile, is_permitted):
+    #print run.strip(), fullname.strip(), card, company_code, company, place, profile, is_permitted
     if card == None:
-        result = db.people.update({'_id':run},{'$set':{"_id": run,"fullname": fullname,"card": 0,"company_code": company_code,"company": company,"place": place,"profile": profile,"is_permitted": is_permitted}},upsert = True)
+        db.people.insert_one({"run":run.strip(),"fullname": fullname.strip(),"card": 0,"company_code": company_code,"company": company,"place": place,"profile": profile,"is_permitted": is_permitted})
     else:
-        result = db.people.update({'_id':run},{'$set':{"_id": run,"fullname": fullname,"card": int(card),"company_code": company_code,"company": company,"place": place,"profile": profile,"is_permitted": is_permitted}},upsert = True)
-    #print result
-    # print fullname + " insertada"
+        db.people.insert_one({"run":run.strip(),"fullname": fullname.strip(),"card": int(card),"company_code": company_code,"company": company,"place": place,"profile": profile,"is_permitted": is_permitted})
+
 
 def get_place(db):
     return db.place.find_one()
@@ -124,9 +124,13 @@ def parseXml(file, profile):
                 place = employee[5].text
                 add_place(db, place, company_code)
                 add_company(db,company, company_code)
+                # Check if already exist.
+                while (db.people.find({"run": { $eq: run }}).count() > 0):
+                    SLACK.notify(text="Person "+run+" already exist!", channel="#multiexportfoods", username="Multi-Boot", icon_emoji=":robot_face:")
+                    db.people.remove({"run": run})
                 add_person(db, run, fullname, card, company_code, company, place, profile="E", is_permitted=True)
-            #SLACK.notify(text="People updated!", channel="#multiexportfoods", username="Multi-Boot", icon_emoji=":robot_face:")
-            print "People updated!"
+            SLACK.notify(text="People updated!", channel="#multiexportfoods", username="Multi-Boot", icon_emoji=":robot_face:")
+            #print "People updated!"
             saveLastFile(file, DIR + LAST_EMPLOYEES)
         else:
             for employee in root.findall('SUBCONTRATISTA'):
@@ -134,11 +138,11 @@ def parseXml(file, profile):
                 fullname = employee[1].text
                 card = employee[2].text
                 company_code = employee.find('company_code').text
-                company = employee[4].text
+                company = employee.find('company').text
                 place = employee[5].text
                 add_person(db, run, fullname, card, company_code, company, place, profile="C", is_permitted=True)
-            #SLACK.notify(text="Contractors updated!", channel="#multiexportfoods", username="Multi-Boot", icon_emoji=":robot_face:")
-            print "People updated!"
+            SLACK.notify(text="Contractors updated!", channel="#multiexportfoods", username="Multi-Boot", icon_emoji=":robot_face:")
+            #print "People updated!"
             saveLastFile(file, DIR + LAST_CONTRACTORS)
 
     except IOError as io:
@@ -160,22 +164,23 @@ def main():
         CO_FILE = max(glob.iglob(DIR + 'SUBCONTRATISTAS*.xml'), key=os.path.getctime)
 
         db = get_db()
-        db.people.delete_many({})
-        db.place.delete_many({})
         if areEquals(EM_FILE, DIR + LAST_EMPLOYEES):
             print "Employees are up to date"
             # SLACK.notify(text="Employees are up to date", channel="#multiexportfoods", username="Multi-Boot", icon_emoji=":robot_face:")
         else:
             # update employees from new xml
+            db.place.drop()
+            db.people.delete_many({"profile": "E"})
             parseXml(EM_FILE, "E")
             # sendGet(EM_FILE)
             sendUpdate()
 
-        if areEquals(CO_FILE, LAST_CONTRACTORS):
+        if areEquals(CO_FILE, DIR + LAST_CONTRACTORS):
             print "Contractors are up to date"
             # SLACK.notify(text="Contractors are up to date", channel="#multiexportfoods", username="Multi-Boot", icon_emoji=":robot_face:")
         else:
             # update contractors from new xml
+            db.people.delete_many({"profile": "C"})
             parseXml(CO_FILE, "C")
             # sendGet(CO_FILE)
             sendUpdate()
